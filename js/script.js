f1b1a0c9c2003ac68ae708c2e3cd689d9c82ae9f
millooo94 <camillomusmeciunict@gmail.com>
2022-11-03 15:43:08 +0100

    primo commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+const eleGrid = document.querySelector('.grid')
+const eleButton = document.querySelector('button')
+const eleLevel = document.getElementById ('level')
+const arrRandoms = []
+
+
+
+
+
+
+function levelSelector(levelIndex) {
+
+    
+
+    for (let i = 1; i <= levelIndex; i++) {
+
+        const eleCell = document.createElement('div')
+        eleCell.innerHTML = i
+        eleCell.classList.add('cell')
+        eleGrid.append(eleCell);
+
+        
+        
+
+        if (levelIndex == 100) {
+
+            
+
+            arrRandoms.push(i)
+
+
+
+            eleCell.style.width = "calc(100% / 10)"
+            eleCell.style.height = "calc(100% / 10)"
+
+        } else if (levelIndex == 81) {
+
+            
+
+            arrRandoms.push(i)
+
+
+            eleCell.style.width = "calc(100% / 9)"
+            eleCell.style.height = "calc(100% / 9)"
+        } else {
+
+            arrRandoms.push(i)
+
+
+            eleCell.style.width = "calc(100% / 7)"
+            eleCell.style.height = "calc(100% / 7)"
+        }
+
+        eleCell.addEventListener('click', function(){
+            this.classList.toggle('active')
+        })
+
+    }
+}
+
+levelSelector (100);
+
+
+eleButton.addEventListener('click', function() {
+
+    eleGrid.replaceChildren()
+
+    let levelIndex
+
+    if (eleLevel.value == 1) {
+        levelIndex = 100
+    } else if (eleLevel.value == 2) {
+        levelIndex = 81
+    } else {
+        levelIndex = 49
+    }
+
+levelSelector(levelIndex);
+
+})
+
+
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+};
+
+
+
