7046f889c9f34be108ec0de61c40b1dae6dae6d4
millooo94 <camillomusmeciunict@gmail.com>
2022-11-03 16:15:31 +0100

    refactor all

--- js/script.js
+++ js/script.js
@@ -6,43 +6,30 @@ const arrRandoms = []
 function levelSelector(levelIndex) {
 
     for (let i = 1; i <= levelIndex; i++) {
-
         const eleCell = document.createElement('div')
         eleCell.innerHTML = i
         eleCell.classList.add('cell')
         eleGrid.append(eleCell);
-
         if (levelIndex == 100) {
-
             eleCell.style.width = "calc(100% / 10)"
             eleCell.style.height = "calc(100% / 10)"
-
         } else if (levelIndex == 81) {
-
             eleCell.style.width = "calc(100% / 9)"
             eleCell.style.height = "calc(100% / 9)"
-
         } else {
-
             eleCell.style.width = "calc(100% / 7)"
             eleCell.style.height = "calc(100% / 7)"
         }
-
         eleCell.addEventListener('click', function(){
             this.classList.toggle('active')
         })
-
     }
 }
 
 levelSelector (100);
-
 eleButton.addEventListener('click', function() {
-
     eleGrid.replaceChildren()
-
     let levelIndex
-
     if (eleLevel.value == 1) {
         levelIndex = 100
     } else if (eleLevel.value == 2) {
@@ -50,10 +37,9 @@ eleButton.addEventListener('click', function() {
     } else {
         levelIndex = 49
     }
-
 levelSelector(levelIndex);
-
 })
+
 
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
