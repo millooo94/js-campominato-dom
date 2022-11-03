10b9b2d72408be1cd95a210c48294033e65a87e2
millooo94 <camillomusmeciunict@gmail.com>
2022-11-03 16:16:39 +0100

    refactor all

--- js/script.js
+++ js/script.js
@@ -40,7 +40,6 @@ eleButton.addEventListener('click', function() {
 levelSelector(levelIndex);
 })
 
-
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 };
